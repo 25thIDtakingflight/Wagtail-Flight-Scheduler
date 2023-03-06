3bea37a6fa931f4fd266f79e216e052225df0f64
SoraTSky <59984412+SoraTSky@users.noreply.github.com>
2023-03-06 09:32:38 -1000

    Fixed FlightScheduler Setup
    --properly set up the wagtail

---
+++ FlightScheduler/FlightScheduler/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-bu($^w^!u67fh160!3&u@nn56+lq=c6z4jp54rkwk09$v8y((k"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
